b61a45019c643eda946ef366847e027c7e973487
promwarm <promwarm@gmail.com>
2018-04-24 21:59:42 +0200

    Creates Stock objects

--- create_stocks.py
+++ create_stocks.py
@@ -2,11 +2,44 @@ import DeGiro
 from Stock import Stock
 
 list_of_txs = DeGiro.processTransactionsFile()
-list_of_stocks = []
+list_of_stocks = {}
 
 for tx in list_of_txs:
-    if tx['product'] not in Stock.all_stocks:
-        s = Stock(tx['product'], tx['date'])
-        list_of_stocks.append(s)
+    # Instantiate Stock object
+    if tx['product'] not in list_of_stocks:
+        list_of_stocks[tx['product']] = Stock(tx['product'], tx['date'])
 
-print(s.number_of_stocks)
+    # When transactions are free, they are returned as empty, instead of 0, we have to fix this
+    if tx['transaction_cost'] == '':
+        tx['transaction_cost'] = 0
+
+    # Adding transaction
+    list_of_stocks[tx['product']].add_transaction(
+        tx['date'],
+        tx['time'],
+        int(tx['shares']),
+        float(tx['foreign_price']),
+        float(tx['transaction_cost'])
+    )
+
+# TODO, this part should be in another function or file
+# Show data
+print(
+    'Stock'.ljust(25),
+    'Shares'.rjust(6),
+    'Cost base'.rjust(7),
+    'Cost / share'.rjust(5)
+)
+
+show_current_holdings_only = True
+
+for key, stock in list_of_stocks.items():
+    if show_current_holdings_only and stock.get_shares() == 0:
+        continue
+
+    print(
+        key.ljust(25) +                                                              # Name
+        str(stock.get_shares()).rjust(6) +                                           # Shares
+        '{0:.2f}'.format(stock.get_total_cost()).rjust(10) +                         # Cost base
+        '{0:.2f}'.format(stock.get_total_cost_per_share()).rjust(10)                 # Cost base, per stock
+         )
