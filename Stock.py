7b08b401fb866ad35394be4ac7723cdfecacfcca
promwarm <promwarm@gmail.com>
2018-05-04 23:58:35 +0200

    Main stock class

--- Stock.py
+++ Stock.py
@@ -1,3 +1,4 @@
+from StockInfo import StockInfo
 
 class Stock():
     """An object representing a single stock"""
@@ -9,7 +10,12 @@ class Stock():
         self.purchase_date = purchase_date
         self.shares = shares
         self.price = price
+
+        # Initiate, but don't set
         self.total_cost = 0
+        self.ticker = ''
+        self.stockinfo = None
+        self.current_price = 0
 
         Stock.number_of_stocks += 1
 
@@ -25,3 +31,23 @@ class Stock():
 
     def get_total_cost_per_share(self):
         return self.total_cost / self.shares
+
+    def set_ticker(self, ticker):
+        self.ticker = ticker.upper()
+
+    def get_price(self):
+        if self.ticker:
+            if self.current_price:
+                return self.current_price
+            else:
+                self.stockinfo = StockInfo(self.ticker)
+                self.current_price = self.stockinfo.getPrice()
+                return self.current_price
+        else:
+            return 0.0
+
+    def setCurrentPrice(self, current_price):
+        self.current_price = current_price
+
+    def setISIN(self, ISIN):
+        self.ISIN = ISIN
