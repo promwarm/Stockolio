79fa2ea4411febe54345fa7f63a38375a1164d25
promwarm <promwarm@gmail.com>
2018-05-07 08:01:11 +0200

    Factory to create Stock objects (WIP)

---
+++ StockFactory.py
@@ -0,0 +1,9 @@
+"""
+Work in progress. This should replace most of create_stock.py, because this class will hold a list
+of Stock class objects.
+"""
+
+class StockFactory():
+
+    def __init__(self):
+
